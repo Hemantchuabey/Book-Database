11db1203128b1298f4b2d7ebbc0abc30a7221fae
Hemant <hementchaubey@gmail.com>
2023-01-13 18:31:45 +0530

    Added dependencies Express and mongoose, made connection and created database

---
+++ app.js
@@ -0,0 +1,14 @@
+import express, { json } from "express";
+import mongoose from "mongoose";
+
+const app = express();
+mongoose.set("strictQuery", true);
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/bookDatabase")
+  .then(app.listen(5000))
+  .then(console.log("Connected to Database and Listening to port 5000"))
+  .catch((err) => {
+    console.log(err);
+  });
