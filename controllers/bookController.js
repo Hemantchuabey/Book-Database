f119b68435aea02fc4a36d1483ddf3e96d1f2eff
Hemant <hementchaubey@gmail.com>
2023-01-13 23:33:20 +0530

    Created addBook route in controller which take body from frontend and check for exisitance of the book if book is not present then it will add the books into the database.

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -12,3 +12,27 @@ export const getAllBooks = async (req, res, next) => {
   }
   return res.status(200).json({ books });
 };
+
+export const addBooks = async (req, res, next) => {
+  const { title, author, description } = req.body;
+  let exisitingBooks;
+  try {
+    exisitingBooks = await bookModel.findOne({ author });
+  } catch (err) {
+    return console.log(err);
+  }
+  if (exisitingBooks) {
+    return res.status(400).json({ message: "Book already exist !!" });
+  }
+  const book = new bookModel({
+    title,
+    author,
+    description,
+  });
+  try {
+    await book.save();
+  } catch (err) {
+    return console.log(err);
+  }
+  return res.status(201).json({ book });
+};
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllBooks } from "../controllers/bookController";
+import { addBooks, getAllBooks } from "../controllers/bookController";
 
 const booksRouter = express.Router();
 
 booksRouter.get("/", getAllBooks);
+booksRouter.post("/addBook", addBooks);
 
 export default booksRouter;
